deed5ba7a18e9d96532ecd3d53e3f4610bf56720
Jeffrey Horn <jeffh@generalassemb.ly>
2016-02-12 12:29:37 -0500

    Add solution

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,7 +1,36 @@
 # frozen_string_literal: true
 
 class FibonacciRange
+  include Enumerable
+
+  def each
+    previous_fib = 0 # || nil
+    current_fib = 0
+    next_fib = 1
+
+    while current_fib <= stop
+      yield current_fib
+
+      previous_fib = current_fib
+      current_fib = next_fib
+      next_fib = current_fib + previous_fib
+    end
+  end
+
+  attr_reader :stop
+  private :stop
+
+  def initialize(stop)
+    @stop = stop
+  end
 end
 
 class FibonacciNumber
+  PHI = (1 + Math.sqrt(5)) / 2
+
+  def self.new(n = 1)
+    FibonacciRange.new(PHI**n).each_with_index do |fib, i|
+      return fib unless (i + 1) < n
+    end
+  end
 end
