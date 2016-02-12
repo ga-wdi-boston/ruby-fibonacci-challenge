351f476a9c55de46f363406336ed3331b6896b0c
mfbrewster <michael.f.brewster@gmail.com>
2016-02-12 15:18:45 -0500

    challenge response

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,5 +1,36 @@
 class FibonacciRange
+  include Enumerable
+  attr_reader :stop
+
+  def initialize(stop)
+    @stop = stop
+  end
+
+  def each()
+    current_fib = 0
+    previous_fib = 0
+    next_fib = 1
+    while current_fib < stop
+      yield current_fib
+      previous_fib = current_fib
+      current_fib = next_fib
+      next_fib = previous_fib + current_fib
+    end
+  end
 end
 
 class FibonacciNumber
+  attr_reader :current_fib
+
+  def FibonacciNumber.new(place = 0)
+    current_fib = 0
+    previous_fib = 0
+    next_fib = 1
+    (place - 1).times do
+      previous_fib = current_fib
+      current_fib = next_fib
+      next_fib = previous_fib + current_fib
+    end
+    return current_fib
+  end
 end
