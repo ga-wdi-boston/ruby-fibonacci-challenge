bcbb892655f2b054f866640f8e884aaf5cfb4cb3
soynog <nicolas.o.garcia@gmail.com>
2016-04-19 09:16:53 -0400

    Implement Fibonacci Range Class
    - Use a two-element array as a seed for producing each fib

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,4 +1,21 @@
+# Produces fibonacci sequences
 class FibonacciRange
+  include Enumerable
+
+  def initialize(stop)
+    @stop = stop
+  end
+
+  def each
+    seed = [0, 1]
+    current = seed[1]
+    yield seed[0]
+    while current <= @stop
+      yield current
+      current = seed.reduce(&:+)
+      seed = [seed[1], current]
+    end
+  end
 end
 
 class FibonacciNumber
