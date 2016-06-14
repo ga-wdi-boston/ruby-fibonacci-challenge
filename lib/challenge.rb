f0108d8ae8bc9524dd43c1e078bf79afd6fe3d01
SarahHartwick <sarah@surfsetfitness.com>
2016-06-14 09:43:08 -0400

    FibonacciRange Passes Test
    Finish first part of challenge

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,5 +1,22 @@
 class FibonacciRange
-end
+  include Enumerable
+  attr_reader :stop, :previous_fib, :current_fib, :next_fib
 
-class FibonacciNumber
+  def each
+    @previous_fib = 0
+    @current_fib = 1
+    @next_fib = 0
+    while @previous_fib <= stop
+      yield @previous_fib
+      @next_fib = @previous_fib + @current_fib
+      @previous_fib = @current_fib
+      @current_fib = @next_fib
+    end
+  end
+
+  def initialize(stop)
+    @stop = stop
+  end
 end
+# class FibonacciNumber
+# end
