5117741e119fd70b8e00e11b5c7f109231238538
SarahHartwick <sarah@surfsetfitness.com>
2016-06-15 15:37:13 -0400

    Passes Tests
    Complete FibonacciNumber using FibonacciRange & without using

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,3 +1,4 @@
+#
 class FibonacciRange
   include Enumerable
   attr_reader :stop, :previous_fib, :current_fib, :next_fib
@@ -19,13 +20,30 @@ class FibonacciRange
   end
 end
 
-class FibonacciNumber
-  def initialize(num = 1)
-    @num = num
-    @fib_range = FibonacciRange.new(@num).to_a
-  end
+#
+# class FibonacciNumber
+#   def self.new(num = 1)
+#     stop = 0
+#     stop += 1 while FibonacciRange.new(stop).to_a.length < num
+#     FibonacciRange.new(stop).to_a[num - 1].to_i
+#   end
+# end
 
-  def to_i
-    @fib_range[@num - 1]
+#
+class FibonacciNumber
+  def self.new(num = 1)
+    array = []
+    previous_fib = 0
+    current_fib = 1
+    next_fib = 0
+    i = 0
+    while i < num
+      array.push previous_fib
+      next_fib = previous_fib + current_fib
+      previous_fib = current_fib
+      current_fib = next_fib
+      i += 1
+    end
+    array[num -1]
   end
 end
