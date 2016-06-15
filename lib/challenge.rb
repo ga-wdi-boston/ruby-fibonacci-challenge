18ece30fad5272df6792c8e9974ad97f2ca7dec1
SarahHartwick <sarah@surfsetfitness.com>
2016-06-15 15:51:31 -0400

    Dry-ed out code
    Shortened methods

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -21,29 +21,19 @@ class FibonacciRange
 end
 
 #
-# class FibonacciNumber
-#   def self.new(num = 1)
-#     stop = 0
-#     stop += 1 while FibonacciRange.new(stop).to_a.length < num
-#     FibonacciRange.new(stop).to_a[num - 1].to_i
-#   end
-# end
+class FibonacciNumber
+  def self.new(num = 1)
+    stop = 0
+    stop += 1 while FibonacciRange.new(stop).to_a.length < num
+    FibonacciRange.new(stop).to_a[num - 1].to_i
+  end
+end
 
 #
 class FibonacciNumber
   def self.new(num = 1)
-    array = []
-    previous_fib = 0
-    current_fib = 1
-    next_fib = 0
-    i = 0
-    while i < num
-      array.push previous_fib
-      next_fib = previous_fib + current_fib
-      previous_fib = current_fib
-      current_fib = next_fib
-      i += 1
-    end
-    array[num -1]
+    array = [0, 1]
+    array.push(array[-1] + array[-2]) until array.length >= num
+    array[num - 1]
   end
 end
