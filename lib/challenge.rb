3b384e5716e0dcf98b9306d44a5d6f6e3c2ef0a6
kopiusmaximus <alexander.kopynec@gmail.com>
2016-09-21 13:31:20 -0400

    Refactor .each
    -break fibonacci logic into separate get_next_fib method

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -18,12 +18,7 @@ class FibonacciRange
     current_fib = 0
     while current_fib <= stop
       yield current_fib
-      # get the next number in the Fibonacci sequence
-      if current_fib.zero?
-        next_fib = 1
-      else
-        next_fib = current_fib + previous_fib
-      end
+      next_fib = get_next_fib(previous_fib, current_fib)
       # advance the state of current and previous fib
       previous_fib = current_fib
       current_fib = next_fib
@@ -31,12 +26,12 @@ class FibonacciRange
   end
 
   def get_next_fib(previous_fib, current_fib)
-
+    current_fib.zero? ? 1 : current_fib + previous_fib
   end
 end
 
 class FibonacciNumber
 end
 
-# binding.pry
-# ''
+binding.pry
+''
