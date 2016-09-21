1fdc48b48277e77458780936f2c3a478949ee3c1
kopiusmaximus <alexander.kopynec@gmail.com>
2016-09-21 15:19:46 -0400

    Implement FibonacciNumbers
    -Implement FibonacciNumbers class, returning nth Fibonacci number
    -pass all rspec tests

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,6 +1,6 @@
 require 'pry'
 
-# FibonacciRange class
+# class for getting a range of Fibonacci numbers
 class FibonacciRange
   include Enumerable
 
@@ -16,8 +16,10 @@ class FibonacciRange
     # start at 0
     previous_fib = 0
     current_fib = 0
+    # advance the sequence until the stop value is reached
     while current_fib <= stop
       yield current_fib
+      # get the next number in the sequence
       next_fib = get_next_fib(previous_fib, current_fib)
       # advance the state of current and previous fib
       previous_fib = current_fib
@@ -25,13 +27,22 @@ class FibonacciRange
     end
   end
 
+  # helper method to get the next number in the Fibonacci sequence
   def get_next_fib(previous_fib, current_fib)
     current_fib.zero? ? 1 : current_fib + previous_fib
   end
 end
 
+# class for getting the nth Fibonacci number
 class FibonacciNumber
+  def self.new(n = 1)
+    get_nth_fib(n)
+  end
+
+  def self.get_nth_fib(n)
+    n == 1 ? 0 : ((1.618**(n - 1) - -0.618**(n - 1)) / 2.236).round
+  end
 end
 
-binding.pry
-''
+# binding.pry
+# ''
