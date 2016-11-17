c423a5f5ba4fb934d7fd75791ea20666368cb27a
treep78 <treep78@gmail.com>
2016-11-17 09:28:24 -0500

    attempt solution

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,4 +1,26 @@
+# FibonacciRange
 class FibonacciRange
+  include Enumerable
+  attr_accessor :stop
+
+  def initialize(stop)
+    @stop = stop
+  end
+
+  def each
+    current_fib = 1
+    previous_fib = 0
+    next_fib = 0
+    self
+    i = 0
+    while i < @stop
+      next_fib = current_fib + previous_fib
+      previous_fib = current_fib
+      current_fib = next_fib
+      self
+      i += 1
+    end
+  end
 end
 
 class FibonacciNumber
