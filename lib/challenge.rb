d68353a757c26c046887adc4caa6819174d80185
joaufi <johnathan.a.fisher@gmail.com>
2016-11-17 09:19:20 -0500

    Complete challenge primary question
    FibonacciRange with a stop initialized

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,5 +1,27 @@
+# Does fibonnaci stuff
 class FibonacciRange
+  include Enumerable
+
+  attr_reader :stop
+
+  def initialize(stop)
+    @stop = stop
+  end
+
+  def each
+    current_fib = 0
+    previous_fib = 1
+    while current_fib < @stop
+      yield current_fib
+      next_fib = current_fib + previous_fib
+      current_fib = next_fib
+      previous_fib = current_fib - previous_fib
+    end
+  end
 end
 
 class FibonacciNumber
 end
+
+binding.pry
+''
