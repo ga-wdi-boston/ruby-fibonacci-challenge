878be79194c4ac685ffdfc4df8e096466f580700
Soboscott <scott.landry01772@yahoo.com>
2017-02-08 09:28:58 -0500

    not finished but would like input on code so far.

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,5 +1,23 @@
+# frozen_string_literal: true
 class FibonacciRange
+  include Comparable
+  attr_accessor :length
+  def initialize(n)
+    @length = n
+ end
 end
 
-class FibonacciNumber
+class FibonacciNumber < FibonacciRange
+  def fibonacci
+    fibonacci.new do |y|
+      a = b = 1
+
+    loop do
+      y << a
+      a, b = b, a + b
+    end
+    end
+  end
+  FibonacciRange.new(10).to_a
+  p FibonacciRange
 end
