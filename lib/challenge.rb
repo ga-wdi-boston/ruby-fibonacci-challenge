4d47b5656f0d99871122b69b43310e398d1303de
moshiko1988 <moshiko1988@gmail.com>
2017-02-08 20:33:14 -0500

    fibonacci challenge honestly it took me a while to understand what to do. i think i got the first part working without the bonus i'd love any recommendation of how to write it better.

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,4 +1,27 @@
+# frozen_string_literal: true
+
+# FibonacciRange class
 class FibonacciRange
+  attr_reader :stop
+
+  include Enumerable
+
+  def initialize(stop)
+    @stop = stop
+  end
+
+  def each
+    current_fib = 0
+    # previous_fib = 0
+    next_fib = 1
+
+    while current_fib <= stop
+      yield current_fib
+      previous_fib = current_fib
+      current_fib = next_fib
+      next_fib = current_fib + previous_fib
+    end
+  end
 end
 
 class FibonacciNumber
