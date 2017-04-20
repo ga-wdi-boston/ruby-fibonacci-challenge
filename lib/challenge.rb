023a95433a6dacb3915a0f6306ace53cb56b9656
jordanallain <jallain@gmail.com>
2017-04-20 14:44:41 -0400

    Add solution code to `lib/challenge.rb`
    Update solution for cohort lm01

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,7 +1,33 @@
 # frozen_string_literal: true
 
+# An enumerable Fibonacci number generator
 class FibonacciRange
+  include Enumerable
+  attr_reader :stop
+  private :stop
+
+  def initialize(stop)
+    @stop = stop
+  end
+
+  def each
+    current = 0
+    successor = 1
+    while current < stop
+      yield current
+      current, successor = successor, current + successor
+    end
+    self
+  end
 end
 
+# Generates the nth Fibonacci number
 class FibonacciNumber
+  PHI = (1 + Math.sqrt(5)) / 2
+
+  def self.new(n = 1)
+    FibonacciRange.new(PHI**n).each_with_index do |fib, i|
+      return fib unless (i + 1) < n
+    end
+  end
 end
