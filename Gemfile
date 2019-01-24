b0c04f04b667e4cab1b1967502c81c297f17b38a
xiaojie zhang <xiaojiezhang@S0106441c120f48e6.ed.shawcable.net>
2019-01-24 09:37:17 -0500

    response to challenge

--- Gemfile
+++ Gemfile
@@ -1,8 +1,15 @@
+# @Author: xiaojiezhang
+# @Date:   2019-01-24T09:00:48-05:00
+# @Last modified by:   xiaojiezhang
+# @Last modified time: 2019-01-24T09:04:50-05:00
+
+
+
 # Defaults for all ruby based repositories
 # frozen_string_literal: true
 
 source 'https://rubygems.org'
-ruby '2.3.1'
+ruby '2.5.0'
 gem 'binding_of_caller'
 gem 'byebug', '~> 9.0'
 gem 'pry', '~> 0.10'
--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,7 +1,36 @@
+# @Author: xiaojiezhang
+# @Date:   2019-01-24T09:00:48-05:00
+# @Last modified by:   xiaojiezhang
+# @Last modified time: 2019-01-24T09:30:48-05:00
+
+
+require 'pry'
 # frozen_string_literal: true
 
 class FibonacciRange
+  include Enumerable
+
+  def each
+  current_fib =0
+  next_fib=1
+    while current_fib < @stop
+        yield current_fib
+        previous = current_fib
+        current_fib = next_fib
+        next_fib= current_fib+previous
+    end
+  end
+
+  attr_reader :stop
+  def initialize(num)
+    @stop = num
+  end
+
+
 end
 
 class FibonacciNumber
 end
+
+binding.pry
+''
