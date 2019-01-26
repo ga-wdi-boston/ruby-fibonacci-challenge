d55c5b8af3cb51b2326dacf7245bb24a670ee6c5
macitege <macitegeercan@gmail.com>
2019-01-25 22:04:19 -0500

    Ege's Response to FibonacciRange

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,7 +1,31 @@
 # frozen_string_literal: true
 
+# Fibonacci Range finder class
 class FibonacciRange
+  include Enumerable
+  attr_accessor :stop
+
+  def initialize(stop_num)
+    @stop = stop_num
+  end
+
+  def each
+    prev_num = 0
+    curr_num = 1
+    0.upto(@stop) do |i|
+      if i <= 1
+        yield i
+      elsif prev_num + curr_num < @stop
+        i = prev_num + curr_num
+        yield prev_num + curr_num
+        prev_num = curr_num
+        curr_num = i
+      end
+    end
+  end
 end
+
+FibonacciRange.new(10).map(&:itself)
 
 class FibonacciNumber
 end
